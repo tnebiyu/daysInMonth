bcc948687994cb0a7b3398f3e62a017450dde02f
nebahabesha <nebiyu28@gmail.com>
2021-07-04 09:37:47 +0300

    it display days in a month

---
+++ src/myClass.java
@@ -0,0 +1,29 @@
+public class myClass {
+    public static void main(String[] args) {
+        getDaysInMonth(2,2010);
+        getDaysInMonth(1, 1998);
+        isLeapYear(1998);
+    }
+
+    public static boolean isLeapYear(int year){
+        if (year < 1 || year > 9999){
+            return false;
+        } return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 );
+
+    }
+    public static int getDaysInMonth (int month, int year){
+        if((month < 1 || month > 12) || (year < 1 || year >9999)){
+            return -1;
+        } if(month == 2) {
+            return isLeapYear(year) ? 29 : 28;
+        }
+
+        switch (month){
+            case 4: case 6: case 9: case 11:
+                return 30;
+
+            default:
+                return 31;
+        }
+    }
+}
